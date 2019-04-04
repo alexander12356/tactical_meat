c780ccc8a3e82d86a45369098cce1f603b131841
ALEXSUPERCOMP\alexa <alexander12356@inbox.ru>
2019-04-04 21:33:23 +0900

    load to server

---
+++ Assets/bullet.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using DG.Tweening;
+
+public class bullet : MonoBehaviour
+{
+    [SerializeField] private float m_Speed = 0f;
+
+    public void Shoot(Vector3 targetPosition)
+    {
+        transform.DOMove(targetPosition, m_Speed).SetEase(Ease.Linear).OnComplete(() => Destroy(gameObject));
+    }
+}
---
+++ Assets/squad.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class squad : MonoBehaviour
+{
+    [SerializeField] private int m_ShootCount = 0;
+    [SerializeField] private float m_ShootDelta = 1f;
+    [SerializeField] private bullet m_BulletPrefab = null;
+    [SerializeField] private Transform m_Transform;
+    [SerializeField] private float m_ShooterRadiusThreshold;
+    [SerializeField] private float m_TargetRadiusThreshold;
+    [SerializeField] private float m_ValleyThreshold;
+
+    private void Start()
+    {
+        Volley();
+    }
+
+    private void Volley()
+    {
+        StartCoroutine(VolleyCoroutine());
+    }
+
+    private IEnumerator VolleyCoroutine()
+    {
+        while (true)
+        {
+            yield return ShootCoroutine(m_ShootCount);
+            yield return new WaitForSeconds(Random.Range(0, m_ValleyThreshold));
+        }
+    }
+
+    private IEnumerator ShootCoroutine(int shootCount)
+    {
+        for (int i = 0; i < shootCount; i++)
+        {
+            var bullet = Instantiate(m_BulletPrefab);
+            bullet.transform.position = transform.position + (Random.insideUnitSphere * m_ShooterRadiusThreshold);
+            bullet.Shoot(m_Transform.position + (Random.insideUnitSphere * m_TargetRadiusThreshold));
+
+            yield return new WaitForSeconds(Random.Range(0, m_ShootDelta));
+        }
+    }
+}
